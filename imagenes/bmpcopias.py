bdbab15935bb8d2c696b6dd251415ec76fb30610
ErwinPo <A01734881@tec.mx>
2025-05-06 16:31:49 -0600

    Copia de imagenes script

---
+++ imagenes/bmpcopias.py
@@ -0,0 +1,14 @@
+import shutil
+
+# Path to the original BMP file
+source_file = 'prueba1.bmp'  # Replace with your actual file path
+
+# Output file name prefix
+output_prefix = 'output_'
+
+# Duplicate the file 100 times
+for i in range(10):
+    output_filename = f'{output_prefix}{i}.bmp'
+    shutil.copy(source_file, output_filename)
+
+print("100 copies created successfully.")
